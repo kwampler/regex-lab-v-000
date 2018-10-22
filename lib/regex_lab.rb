ced9f7161cba07d70552f2f4e7f4f8e11dfe5467
katejsenger <katejsenger@yahoo.com>
2018-10-22 02:43:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if word.match(\/^[A-Z].*[a-zA-Z][.]$/\)
+  if word.match(/^[A-Z].*[a-zA-Z][.]$/)
     return true
   else
     return false
