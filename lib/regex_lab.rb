3d9ecb219ee9d62a46ad8095e707640b08321ed5
katejsenger <katejsenger@yahoo.com>
2018-10-22 02:40:09 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if word.match()
+    return true
+  else
+    return false
 end
 
 def valid_phone_number?(phone)
